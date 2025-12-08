e21500ad1bbcc42cb3a64eec571e2775265889e1
Aniket Baranwal <aniket.baranwal@incture.com>
2025-12-08 10:37:42 +0530

    Added initial register form with controller

--- app/todoapp/webapp/controller/Register.controller.js
+++ app/todoapp/webapp/controller/Register.controller.js
@@ -1,10 +1,20 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+  "use strict";
 
-    return Controller.extend("my.todo.todoapp.controller.Register", {
-        onInit() {
-        }
-    });
+  return Controller.extend("my.todo.todoapp.controller.Register", {
+    onInit: function () {
+      const oModel = new sap.ui.model.json.JSONModel({
+        name: "",
+        email: "",
+        password: "",
+      });
+
+      this.getView().setModel(oModel, "register");
+    },
+    onRegisterPress: function () {
+      const oData = this.getView().getModel("register").getData();
+
+      console.log("Register Form Data:", oData);
+    },
+  });
 });
