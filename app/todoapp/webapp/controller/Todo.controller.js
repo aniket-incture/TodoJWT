4df40e6c9f28c7f546870439ec76b193fec9d2d2
Aniket Baranwal <aniket.baranwal@incture.com>
2025-12-08 15:06:09 +0530

    Added loadTodo on route matching controller

--- app/todoapp/webapp/controller/Todo.controller.js
+++ app/todoapp/webapp/controller/Todo.controller.js
@@ -15,8 +15,13 @@ sap.ui.define(
         });
 
         this.getView().setModel(oModel, "todo");
-      },
+        this.getView().setModel(new JSONModel({ value: [] }), "todos");
 
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("Todo")
+          .attachPatternMatched(this._onRouteMatched, this);
+      },
       onCreateTodo: async function () {
         const oView = this.getView();
         const oModel = oView.getModel("todo");
@@ -68,6 +73,38 @@ sap.ui.define(
           oView.setBusy(false);
         }
       },
+      _onRouteMatched: async function () {
+        await this.loadTodos();
+      },
+      loadTodos: async function () {
+        const oView = this.getView();
+        const oTodosModel = oView.getModel("todos");
+        console.log("Loading todos...");
+        oView.setBusy(true);
+
+        try {
+          const res = await fetch("/odata/v4/todo/Todos", {
+            method: "GET",
+            credentials: "include",
+          });
+          console.log("Fetch todos response status:", res);
+          if (!res.ok) {
+            MessageBox.error("Failed to load todos");
+            return;
+          }
+          console.log("Fetch todos response ok");
+          const data = await res.json();
+          console.log("Fetched todos:", data);
+
+          oTodosModel.setData(data);
+        } catch (err) {
+          console.error(err);
+          MessageBox.error("Unexpected error while loading todos");
+        } finally {
+          oView.setBusy(false);
+        }
+      },
+       
     });
   }
 );
