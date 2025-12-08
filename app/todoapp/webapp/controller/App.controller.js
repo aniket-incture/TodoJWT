8b0d8d153d9f05b64b7d83af6c380c87a60631fa
Aniket Baranwal <aniket.baranwal@incture.com>
2025-12-08 10:24:27 +0530

    Resolved routing path bug and added initial pages content

--- app/todoapp/webapp/controller/App.controller.js
+++ app/todoapp/webapp/controller/App.controller.js
@@ -1,10 +1,19 @@
-sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
+sap.ui.define(["sap/ui/core/mvc/Controller"], (BaseController) => {
   "use strict";
 
   return BaseController.extend("my.todo.todoapp.controller.App", {
-      onInit() {
-      }
+    onInit() {},
+    onGoToLogin: function () {
+      const oRouter = this.getOwnerComponent().getRouter();
+      oRouter.navTo("Login");
+    },
+    onGoToRegister: function () {
+      const oRouter = this.getOwnerComponent().getRouter();
+      oRouter.navTo("Register");
+    },
+    onGoToTodo: function () {
+      const oRouter = this.getOwnerComponent().getRouter();
+      oRouter.navTo("Todo");
+    },
   });
 });
