5576027cf20d0a18ec4690480e441da378c2745e
Aniket Baranwal <aniket.baranwal@incture.com>
2025-12-08 12:37:42 +0530

    Login worked checkpoint

--- app/todoapp/webapp/controller/Login.controller.js
+++ app/todoapp/webapp/controller/Login.controller.js
@@ -1,35 +1,82 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+  ],
+  (Controller, MessageToast, MessageBox, JSONModel) => {
+    "use strict";
 
-  return Controller.extend("my.todo.todoapp.controller.Login", {
-    onInit: function () {
-      const oModel = new sap.ui.model.json.JSONModel({
-        email: "",
-        password: "",
-      });
+    return Controller.extend("my.todo.todoapp.controller.Login", {
+      onInit: function () {
+        const oModel = new sap.ui.model.json.JSONModel({
+          email: "",
+          password: "",
+        });
 
-      this.getView().setModel(oModel, "login");
-    },
+        this.getView().setModel(oModel, "login");
+      },
 
-    onLoginPress: function () {
-      const oData = this.getView().getModel("login").getData();
+      onLoginPress: async function () {
+        const oView = this.getView();
+        const oModel = oView.getModel("login");
+        const oData = { ...oModel.getData() };
 
-      if (!oData.email || !oData.password) {
-        sap.m.MessageToast.show("Please fill all the fields");
-        return;
-      }
-      if(oData.password.length<6){
-        sap.m.MessageToast.show("Password must be at least 6 characters long");
-        return;
-      }
-      console.log("Logging in with", oData);
-      const oModel = this.getView().getModel("login");
-       oModel.setData({ email: "", password: "" });
+        if (!oData.email || !oData.password) {
+          MessageToast.show("Please fill all the fields");
+          return;
+        }
+        if (oData.password.length < 6) {
+          MessageToast.show("Password must be at least 6 characters");
+          return;
+        }
 
-    },
-    onGoToRegister:function(){
+        oView.setBusy(true);
+
+        try {
+          const res = await fetch("/odata/v4/auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify(oData),
+          });
+
+          console.log("login response status:", res.status);
+
+          if (!res.ok) {
+            let errMsg = `Login failed (${res.status})`;
+            try {
+              const errJson = await res.json();
+              errMsg =
+                errJson?.error?.message?.value ||
+                errJson?.error?.message ||
+                errJson?.message ||
+                errMsg;
+            } catch (err) {}
+
+            MessageBox.error(errMsg);
+            return;
+          }
+
+          const result = await res.json();
+          console.log("login success body:", result);
+
+          MessageToast.show("Login successful");
+          oModel.setData({ email: "", password: "" });
+
+        //   this.getOwnerComponent().getRouter().navTo("Todo");
+        } catch (err) {
+          console.error("Unexpected login error:", err);
+          MessageBox.error("Unexpected error during login.");
+        } finally {
+          oView.setBusy(false);
+        }
+      },
+      onGoToRegister: function () {
         const oRouter = this.getOwnerComponent().getRouter();
         oRouter.navTo("Register");
-    }
-  });
-});
+      },
+    });
+  }
+);
