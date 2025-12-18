509e672ce975cdd3d928c72a11129b7e06d64e58
Aniket Baranwal <aniket.baranwal@incture.com>
2025-12-18 12:48:54 +0530

    Mock auth worked with db fix

--- srv/todoService.js
+++ srv/todoService.js
@@ -1,82 +1,57 @@
 const cds = require("@sap/cds");
 
 module.exports = cds.service.impl(function () {
-  this.before("READ", "Todos", async (req) => {
+
+  this.before("READ", "Todos", (req) => {
     if (!req.params || req.params.length === 0) {
-      req.query.where({ owner_ID: req.user.id });
-      return;
-    }
-
-    const id = req.params[0].ID || req.params[0];
-    if (!id) req.reject(400, "Missing entity key");
-    // console.log("This is user id",req.user);
-    const todo = await SELECT.one.from("my.todo.Todo").where({ ID: id });
-
-    if (!todo) req.reject(404, "Todo not found");
-
-    if (todo.owner_ID !== req.user.id) {
-      req.reject(403, "Forbidden — not the owner");
+      req.query.where({ createdBy: req.user.id });
     }
   });
 
   this.before("CREATE", "Todos", (req) => {
-    req.data.owner_ID = req.user.id;
+
     req.data.isDone = false;
-    // console.log("This is user id",req.user);
   });
 
   this.before("UPDATE", "Todos", async (req) => {
     const id =
-      (req.params && req.params[0] && (req.params[0].ID || req.params[0])) ||
+      req.params?.[0]?.ID ??
+      req.params?.[0] ??
       req.data?.ID;
 
     if (!id) req.reject(400, "Missing entity key");
 
-    const todo = await SELECT.one.from("my.todo.Todo").where({ ID: id });
+    const todo = await SELECT.one
+      .from("my.todo.Todo")
+      .where({ ID: id });
 
     if (!todo) req.reject(404, "Todo not found");
-    if (todo.owner_ID !== req.user.id) {
-      req.reject(403, "Forbidden — not the owner");
+
+    if (todo.createdBy !== req.user.id) {
+      req.reject(403, "Forbidden — not your Todo");
     }
 
-    const allowedPayload = {};
-
-    if ("owner_ID" in req.data) {
-      req.reject(400, "Updating 'owner_ID' is not allowed");
+    if ("createdBy" in req.data || "createdAt" in req.data) {
+      req.reject(400, "Managed fields cannot be updated");
     }
-
-    if ("title" in req.data) {
-      allowedPayload.title = req.data.title;
-    }
-
-    // if ("isDone" in req.data) {
-    //   allowedPayload.isDone =
-    //     req.data.isDone === true || req.data.isDone === "true";
-    // }
-
-    req.data.isDone = true;
-    if (Object.keys(allowedPayload).length === 0) {
-      req.reject(
-        400,
-        "Nothing to update — only 'title' or 'isDone' can be updated"
-      );
-    }
-
-    req.data = allowedPayload;
   });
 
   this.before("DELETE", "Todos", async (req) => {
     const id =
-      (req.params && req.params[0] && (req.params[0].ID || req.params[0])) ||
-      req.data?.ID;
+      req.params?.[0]?.ID ??
+      req.params?.[0];
 
     if (!id) req.reject(400, "Missing entity key");
 
-    const todo = await SELECT.one.from("my.todo.Todo").where({ ID: id });
+    const todo = await SELECT.one
+      .from("my.todo.Todo")
+      .where({ ID: id });
 
     if (!todo) req.reject(404, "Todo not found");
-    if (todo.owner_ID !== req.user.id) {
-      req.reject(403, "Forbidden — not the owner");
+
+    if (todo.createdBy !== req.user.id) {
+      req.reject(403, "Forbidden — not your Todo");
     }
   });
+
 });
--- srv/userService.cds
+++
@@ -1,7 +0,0 @@
-using {my.user as user} from '../db/user';
-
-service userService {
-@readonly
-  entity Users as projection on user.User;
-
-}
